96624166ea486c4450fb244f7e147d74db5238e9
Yana-Shrom <dertilemene15@gmail.com>
2024-10-31 13:13:35 +0100

    internal point of variable2

--- src/models/model.c
+++ src/models/model.c
@@ -10,9 +10,9 @@ Card create_random_card() {
 int can_play_card(Player *player, Card card, int row, int col) {
     if (row >= 3 || col >= 5 || player->tableau[row][col].level != 0)
         return 0;
-    if ((card.level == LEVEL_1 && row != 0) ||
-        (card.level == LEVEL_2 && (row != 1 || player->tableau[0][col].level != LEVEL_1)) ||
-        (card.level == LEVEL_3 && (row != 2 || player->tableau[1][col].level != LEVEL_2)))
+    if ((card.level == LEVEL_1 && row != 1) ||
+        (card.level == LEVEL_2 && (row != 2 || player->tableau[0][col].level != LEVEL_1)) ||
+        (card.level == LEVEL_3 && (row != 3 || player->tableau[1][col].level != LEVEL_2)))
         return 0;
     return 1;
 }
