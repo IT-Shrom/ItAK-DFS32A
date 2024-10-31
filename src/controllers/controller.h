17d86141a1484871fd25407229754895e9fe719b
Leméne Cédric <c.lemene@it-students.fr>
2024-10-31 14:12:31 +0100

    Delete src directory

--- src/controllers/controller.h
+++
@@ -1,5 +0,0 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-#include "../models/model.h"
-void game_loop(Player *player1, Player *player2);
-#endif
--- src/views/view.h
+++
@@ -1,8 +0,0 @@
-#ifndef VIEW_H
-#define VIEW_H
-#include "../models/model.h"
-
-void print_card(Card card);
-void display_game_state(Player *player1, Player *player2);
-
-#endif
